4d17efecfc76f784a45fae7deaecc1d669a9b2d9
melih.dal <melih.dal@eatron.com>
2020-06-15 16:34:31 +0300

    master script to run simstar and training added.

---
+++ examples/pytorch/master.py
@@ -0,0 +1,47 @@
+import os
+import threading 
+import time
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(CURRENT_DIR)
+
+"""
+This script assumes that you have the following structure
+
+    train.py
+    master.py
+    SimStar
+    --SimStar.sh
+"""
+
+from train import train
+
+
+
+SIMSTAR_SH_RELATIVE_PATH = 'SimStar/SimStar.sh'
+
+
+def start_sh_script():
+    sh_command = 'sh '+  CURRENT_DIR+SIMSTAR_SH_RELATIVE_PATH
+    os.popen()
+
+if __name__ == "__main__": 
+    # creating thread 
+    simstar_start_thread = threading.Thread(target=start_sh_script, args=(,)) 
+    train_thread = threading.Thread(target=train, args=(,)) 
+  
+    # starting thread 1 
+    simstar_start_thread.start()
+    
+    time.sleep(10) 
+    
+    # starting thread 2 
+    train_thread.start() 
+  
+
+    # wait until thread 2 is completely executed 
+    train_thread.join() 
+  
+    # both threads completely executed 
+    print("Done!") 
